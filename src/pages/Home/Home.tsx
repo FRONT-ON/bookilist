f41040ee1d22edd4b4add8aaa1f54ef4f0d42316
framika <sonia.freydkina@gmail.com>
2023-06-18 19:42:04 +0300

    book slider final
    book slider 4.0 without background2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
-import { Box as MuiBox, Typography as MuiTypography, styled } from '@mui/material';
+import { Box as MuiBox, Typography as MuiTypography, styled, containerClasses } from '@mui/material';
 import { memo } from 'react';
 import { Container } from '@atoms/Container';
+import { BookCarousel } from '@pages/Home/BookCarousel/BookCarousel';
 
 const Box = styled(MuiBox)(() => ({
 	height: '100%',
 	position: 'relative',
-	top: '40%',
+	top: '5%',
 }));
 
 const Typography = styled(MuiTypography)(({ theme }) => ({
@@ -20,12 +21,28 @@ const Typography = styled(MuiTypography)(({ theme }) => ({
 	},
 }));
 
+const ContainerHome = styled(Container)(() => ({
+	[`&.${containerClasses.root}`]: {
+		padding: '0 5%',
+	},
+}));
+
+const ContainerHomeCarousel = styled(Container)(() => ({
+	[`&.${containerClasses.root}`]: {
+		padding: '15% 1.9% 3% 1.9%',
+	},
+}));
+
 export const Home = memo(() => {
 	return (
 		<Box>
-			<Container>
+			<ContainerHome className={containerClasses.root}>
 				<Typography variant="h1">Your online library of favorite books</Typography>
-			</Container>
+			</ContainerHome>
+			<ContainerHomeCarousel className={containerClasses.root}>
+				<BookCarousel filter="bestsellers" text="Bestsellers" />
+				<BookCarousel filter="new" text="New books" />
+			</ContainerHomeCarousel>
 		</Box>
 	);
 });
