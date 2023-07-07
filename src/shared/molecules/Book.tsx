6e93d97cae1ff4ebc44454f6fd0dfc1d0a0fb1c8
framika <sonia.freydkina@gmail.com>
2023-07-07 18:55:53 +0300

    book slider 3.0

--- src/shared/molecules/Book.tsx
+++ src/shared/molecules/Book.tsx
@@ -1,16 +1,16 @@
 import { Card, styled, Typography, Typography as MuiTypography } from '@mui/material';
 import { memo } from 'react';
 import { Book as BookType } from '../../mock/booksMock';
+import { NavigationLink } from '@molecules/NavigationLink';
 
 const BackBook = styled(Card)(({ theme }) => ({
 	background: theme.palette.secondary.main,
 	boxShadow: theme.shadows[3],
-	height: '350px',
-	width: '245px',
+	height: 350,
+	width: 245,
 	display: 'flex',
 	flexDirection: 'column',
-	borderRadius: '6px',
-	cursor: 'pointer',
+	borderRadius: 6,
 	justifyContent: 'center',
 	alignItems: 'center',
 }));
@@ -22,30 +22,30 @@ interface BookImgProps {
 const BookImg = styled('img')<BookImgProps>(({ cover }) => ({
 	backgroundImage: `url(${cover})`,
 	backgroundSize: 'cover',
-	borderRadius: '10px',
-	height: '242px',
-	width: '169px',
+	borderRadius: 10,
+	height: 242,
+	width: 169,
 }));
 
 const BookText = styled(MuiTypography)(({ theme }) => ({
 	fontFamily: theme.typography.fontFamily,
 	fontWeight: 300,
-	lineHeight: '18px',
+	lineHeight: 18,
 	textAlign: 'center',
-	letterSpacing: '0.63px',
-	maxWidth: '180px',
-	marginTop: '13px',
+	letterSpacing: 0.63,
+	maxWidth: 180,
+	marginTop: 13,
 	whiteSpace: 'nowrap',
 
 	'& .title': {
-		fontSize: '16px',
-		height: '44px',
+		fontSize: 16,
+		height: 44,
 		overflow: 'hidden',
 		textOverflow: 'ellipsis',
 	},
 	'& .author': {
-		fontSize: '12px',
-		height: '26px',
+		fontSize: 12,
+		height: 26,
 		overflow: 'hidden',
 		textOverflow: 'ellipsis',
 	},
@@ -53,13 +53,15 @@ const BookText = styled(MuiTypography)(({ theme }) => ({
 
 export const Book = memo(({ book }: { book: BookType }) => {
 	return (
-		<BackBook>
-			<BookImg cover={book.cover} />
-			<BookText className="bookText">
-				<Typography className="title">{book.title}</Typography>
-				<Typography className="author">{book.author} </Typography>
-			</BookText>
-		</BackBook>
+		<NavigationLink to="/bookID">
+			<BackBook>
+				<BookImg cover={book.cover} />
+				<BookText className="bookText">
+					<Typography className="title">{book.title}</Typography>
+					<Typography className="author">{book.author} </Typography>
+				</BookText>
+			</BackBook>
+		</NavigationLink>
 	);
 });
 
