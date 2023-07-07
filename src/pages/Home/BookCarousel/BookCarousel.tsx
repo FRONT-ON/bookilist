72025e3c6929a4b95a05ad5819b9e152d2f76b7f
framika <sonia.freydkina@gmail.com>
2023-07-07 18:58:47 +0300

    book slider 3.0

--- src/pages/Home/BookCarousel/BookCarousel.tsx
+++ src/pages/Home/BookCarousel/BookCarousel.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { Box, styled, Typography as MuiTypography } from '@mui/material';
 import { booksMock } from '../../../mock/booksMock';
 import { Carousel } from '@organisms/Carousel';
-import { Link } from '@atoms/Link';
+import { NavigationLink } from '@molecules/NavigationLink';
 
 interface Props {
 	filter: string;
@@ -13,9 +13,8 @@ const CarouselBox = styled(Box)(({ theme }) => ({
 	border: `7px solid ${theme.palette.secondary.main}`,
 	margin: '50px 0',
 	padding: '3.5%',
-	borderRadius: '15px',
+	borderRadius: 15,
 	boxShadow: theme.shadows[3],
-
 	backgroundColor: theme.palette.secondary.dark,
 }));
 
@@ -30,22 +29,22 @@ const CarouselText = styled(MuiTypography)(({ theme }) => ({
 	color: theme.palette.primary.main,
 	fontWeight: '500',
 	'& .seeAll': {
-		fontSize: '21px',
+		fontSize: 21,
 		[theme.breakpoints.down('md')]: {
-			fontSize: '18px',
+			fontSize: 18,
 		},
 		[theme.breakpoints.down('sm')]: {
-			fontSize: '15px',
+			fontSize: 15,
 		},
 	},
 
 	'& .header': {
-		fontSize: '40px',
+		fontSize: 40,
 		[theme.breakpoints.down('md')]: {
-			fontSize: '30px',
+			fontSize: 30,
 		},
 		[theme.breakpoints.down('sm')]: {
-			fontSize: '20px',
+			fontSize: 20,
 		},
 	},
 }));
@@ -56,8 +55,10 @@ export const BookCarousel = memo(({ filter, text }: Props) => {
 	return (
 		<CarouselBox>
 			<CarouselText>
-				<span className={'header'}>{text}</span>
-				<Link className="seeAll">See all</Link>
+				<span className="header">{text}</span>
+				<NavigationLink to="/books" className="seeAll">
+					See all
+				</NavigationLink>
 			</CarouselText>
 			<Carousel books={filteredBooks} />
 		</CarouselBox>
